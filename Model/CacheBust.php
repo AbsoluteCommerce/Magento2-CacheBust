dcb5a7453bbd27c076ce61b714d0e7d9422e98d3
Joseph McDermott <code@josephmcdermott.co.uk>
2017-02-22 12:19:42 +0000

    Removed per-website and per-store functionality as overkill and doesn't make sense for most (if not all) use-cases

--- Model/CacheBust.php
+++ Model/CacheBust.php
@@ -5,15 +5,11 @@
 namespace Absolute\CacheBust\Model;
 
 use Magento\Framework\App\Cache\TypeListInterface;
-use Magento\Framework\App\Config\Value as ConfigValue;
 use Magento\Framework\App\Config\ConfigResource\ConfigInterface;
-use Magento\Config\Model\ResourceModel\Config\Data\Collection as ConfigCollection;
-use Magento\Config\Model\ResourceModel\Config\Data\CollectionFactory as ConfigCollectionFactory;
 use Magento\PageCache\Model\Cache\Type as PageCache;
 use Magento\Framework\App\Cache\Type\Config as ConfigCache;
 use Magento\Framework\App\Cache\Type\Block as BlockCache;
 use Absolute\CacheBust\Model\Config as CacheBustConfig;
-use Absolute\CacheBust\Model\Source\YesNo;
 
 class CacheBust
 {
@@ -27,24 +23,18 @@ class CacheBust
     /** @var ConfigInterface */
     private $config;
 
-    /** @var ConfigCollectionFactory */
-    private $configCollectionFactory;
-
     /** @var TypeListInterface */
     private $cacheTypeList;
 
     /**
      * @param ConfigInterface $config
-     * @param ConfigCollectionFactory $configCollectionFactory
      * @param TypeListInterface $cacheTypeList
      */
     public function __construct(
         ConfigInterface $config,
-        ConfigCollectionFactory $configCollectionFactory,
         TypeListInterface $cacheTypeList
     ) {
         $this->config = $config;
-        $this->configCollectionFactory = $configCollectionFactory;
         $this->cacheTypeList = $cacheTypeList;
     }
 
@@ -64,10 +54,7 @@ class CacheBust
      */
     public function bustStatic($clearCache = true)
     {
-        $this->_updateValues(
-            CacheBustConfig::XML_PATH_STATIC_ENABLED,
-            CacheBustConfig::XML_PATH_STATIC_VALUE
-        );
+        $this->_updateValue(CacheBustConfig::XML_PATH_STATIC_VALUE);
         
         if ($clearCache) {
             $this->clearCache();
@@ -79,10 +66,7 @@ class CacheBust
      */
     public function bustMedia($clearCache = true)
     {
-        $this->_updateValues(
-            CacheBustConfig::XML_PATH_MEDIA_ENABLED,
-            CacheBustConfig::XML_PATH_MEDIA_VALUE
-        );
+        $this->_updateValue(CacheBustConfig::XML_PATH_MEDIA_VALUE);
 
         if ($clearCache) {
             $this->clearCache();
@@ -100,48 +84,15 @@ class CacheBust
     }
 
     /**
-     * @param string $enabledPath
      * @param string $valuePath
      */
-    private function _updateValues($enabledPath, $valuePath)
+    private function _updateValue($valuePath)
     {
-        $this->_deleteExistingValues($valuePath);
-        
-        /** @var ConfigCollection $configCollection */
-        $configCollection = $this->configCollectionFactory->create();
-        $configCollection->addFieldToFilter('path', $enabledPath);
-        $configCollection->addFieldToFilter('value', YesNo::OPTION_YES);
-        
-        $timestamp = date('YmdHis');
-        foreach ($configCollection as $_configValue) {
-            /** @var ConfigValue $_configValue */
-
-            $this->config->saveConfig(
-                $valuePath,
-                $timestamp,
-                $_configValue->getScope(),
-                (int)$_configValue->getScopeId()
-            );
-        }
-    }
-
-    /**
-     * @param string $valuePath
-     */
-    private function _deleteExistingValues($valuePath)
-    {
-        /** @var ConfigCollection $configCollection */
-        $configCollection = $this->configCollectionFactory->create();
-        $configCollection->addFieldToFilter('path', $valuePath);
-
-        foreach ($configCollection as $_configValue) {
-            /** @var ConfigValue $_configValue */
-
-            $this->config->deleteConfig(
-                $_configValue->getPath(),
-                $_configValue->getScope(),
-                (int)$_configValue->getScopeId()
-            );
-        }
+        $this->config->saveConfig(
+            $valuePath,
+            date('YmdHis'),
+            'default',
+            0
+        );
     }
 }
